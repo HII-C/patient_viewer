f9a7b20cadd2aa398b16832db3309b30d8c16ce0
amichne <austinmichne@gmail.com>
2017-03-17 13:01:04 -0700

    Error Fixing

--- src/app/components/chart.component.ts
+++ src/app/components/chart.component.ts
@@ -1,20 +1,22 @@
 import {Component} from '@angular/core';
-import { CHART_DIRECTIVES } from 'ng2-charts';
+import {ChartsModule} from 'ng2-charts';
+
 @Component({
     selector: 'chart',
     templateUrl: '/chart.html'
 })
+
 export class ChartComponent {
-    public lineChartData: Array<any> = [
+    public data: Array<any> = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
         {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
         ];
-    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    public lineChartOptions: any = {
+    public labels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public options: any = {
             responsive: true
         };
-    public lineChartColors:Array<any> = [
+    public colors:Array<any> = [
         { // grey
           backgroundColor: 'rgba(148,159,177,0.2)',
           borderColor: 'rgba(148,159,177,1)',
@@ -39,8 +41,12 @@ export class ChartComponent {
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgba(148,159,177,0.8)'}
     ];
-    public lineChartLegend: boolean = true;
-    public lineChartType: string = 'line';
+    public legend: boolean = true;
+    public type: string = 'line';
+
+    constructor(){
+        console.log("Chart Component is loaded...");
+    }
 
     test(e: any){
         console.log("Events work, see~" + e);

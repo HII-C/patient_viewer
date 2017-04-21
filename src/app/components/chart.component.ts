2bc98298025db9cdfcf2b58c07f1caa1c8da1b0c
David Gutierrez <david.c.gutierrez@gmail.com>
2017-04-21 15:26:18 -0700

    Separated chart data

--- src/app/components/chart.component.ts
+++ src/app/components/chart.component.ts
@@ -2,65 +2,7 @@ import {Component, Output, EventEmitter, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {LoupeService} from '../services/loupe.service';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
-
-export var single = [
-  {
-    "name": "Germany",
-    "value": 8940000
-  },
-  {
-    "name": "USA",
-    "value": 5000000
-  },
-  {
-    "name": "France",
-    "value": 7200000
-  }
-];
-
-export var multi = [
-  {
-    "name": "Germany",
-    "series": [
-      {
-        "name": "2010",
-        "value": 7300000
-      },
-      {
-        "name": "2011",
-        "value": 8940000
-      }
-    ]
-  },
-
-  {
-    "name": "USA",
-    "series": [
-      {
-        "name": "2010",
-        "value": 7870000
-      },
-      {
-        "name": "2011",
-        "value": 8270000
-      }
-    ]
-  },
-
-  {
-    "name": "France",
-    "series": [
-      {
-        "name": "2010",
-        "value": 5000002
-      },
-      {
-        "name": "2011",
-        "value": 5800000
-      }
-    ]
-  }
-];
+import {single, multi} from './chart_data.component';
 
 
 @Component({
@@ -71,9 +13,11 @@ export var multi = [
 
 export class ChartComponent {
 
+    //Chart Options
+
     single: any[];
     multi: any[];
-    // options
+
     showXAxis = true;
     showYAxis = true;
     gradient = false;
@@ -82,13 +26,13 @@ export class ChartComponent {
     xAxisLabel = 'Country';
     showYAxisLabel = true;
     yAxisLabel = 'Population';
+    view: any[] = [800, 400];
 
     colorScheme = {
       domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
     };
     autoScale = true;
 
-    view: any[] = [800, 400];
 
     public data: Array<any> = [
         {data: [0], label: 'Series A'},

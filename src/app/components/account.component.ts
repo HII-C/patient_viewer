ede572a180d555223e85ad9ac16033dcf1914e3d
cmajorb <cmajorb@gmail.com>
2017-05-10 16:41:15 -0700

    Added UI to the account page

--- src/app/components/account.component.ts
+++ src/app/components/account.component.ts
@@ -1,12 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {DoctorService} from '../services/doctor.service';
 
+
 @Component({
     selector: 'account',
-    templateUrl: '/account.html'
+    templateUrl: '/account.html',
 })
 export class AccountComponent {
     settings: Array<String> = [];
+    electrolytes: Array<String> = ["Sodium","Potassium","Chloride","CO2","Calcium","Magnesium","HBA1C","GLU","ANA"];
+    cholesteralBattery: Array<String> = ["Total","LDL","HDL","Triglycerides"];
+    liverFunction: Array<String> = ["ALT","AST","Albumin","Bilirubin"];
+    kidneyFunction: Array<String> = ["Creatinine","GFR","BUN"];
+
 
     constructor(private doctorService: DoctorService) {
         console.log("AccountComponent created...");

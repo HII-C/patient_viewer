b838b5f0f33237e39484d87a1bee11e90c7667cf
cmajorb <cmajorb@gmail.com>
2017-05-17 12:06:07 -0700

    smart service

--- src/app/services/smart.service.ts
+++ src/app/services/smart.service.ts
@@ -2,30 +2,6 @@ import {Component, Injectable} from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {CookieService} from 'angular2-cookie/core';
 import {FhirService} from '../services/fhir.service';
-<<<<<<< HEAD
-import {ServerService} from '../services/server.service';
-import {PatientService} from '../services/patient.service';
-import {Md5} from 'ts-md5/dist/md5';
-
-@Component({
-    selector: 'smart',
-    templateUrl: ''
-})
-export class SmartService {
-    fhirBaseUrl: string;
-    authorizeUrl: string;
-    tokenUrl: string;
-    clientId: string = "22e46ae8-7adb-42d8-a92c-ce9e9067b06e";
-    launch: string;
-    scope: string = "launch patient/*.* openid profile";
-    redirectUri: string = "http://localhost:9000";
-    state: string = "test";
-    aud: string;
-
-    constructor(private fhirService: FhirService, private patientService: PatientService, private http: Http, private cookieService: CookieService) {
-        console.log("AppComponent has been initialized.");
-    }
-=======
 import {PatientService} from '../services/patient.service';
 import {Md5} from 'ts-md5/dist/md5';
 
@@ -46,8 +22,6 @@ export class SmartService {
     state: string;
     aud: string;
 
->>>>>>> Dev
-
     authenticate() {
       this.fhirBaseUrl = this.findGetParameter("iss");
       if(this.fhirBaseUrl) {
@@ -56,30 +30,17 @@ export class SmartService {
 
         this.fhirService.setUrl(this.fhirBaseUrl);
         this.patientService.setPath("/metadata");
-<<<<<<< HEAD
-        this.patientService.index().subscribe(data => {
-            this.authorizeUrl = data.rest[0].security.extension[0].extension[0].valueUri;
-            this.tokenUrl = data.rest[0].security.extension[0].extension[1].valueUri;
-            this.cookieService.put('tokenUrl', this.tokenUrl);
-=======
         this.patientService.index(false).subscribe(data => {
             this.authorizeUrl = data.rest[0].security.extension[0].extension[0].valueUri;
             this.tokenUrl = data.rest[0].security.extension[0].extension[1].valueUri;
             this.cookieService.put('tokenUrl', this.tokenUrl);
             this.cookieService.put('fhirBaseUrl', this.fhirBaseUrl);
->>>>>>> Dev
             this.requestAuth();
         });
       }
       else {
         if (this.cookieService.get('state') == this.findGetParameter('state')){
-<<<<<<< HEAD
-            console.log("Verification working");
-            console.log(this.state);
-            this.getToken();
-=======
             return this.getToken();
->>>>>>> Dev
           }
         else{
           console.log('Stop cross-site scripting please, thanks');
@@ -90,30 +51,13 @@ export class SmartService {
     }
 
     getToken() {
-<<<<<<< HEAD
-      console.log("getting token");
-=======
->>>>>>> Dev
       var code = this.findGetParameter("code");
       var body = 'code='+code+'&redirect_uri='+encodeURI(this.redirectUri)+'&token_url='+this.cookieService.get('tokenUrl');
 
       var headers = new Headers();
       headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-<<<<<<< HEAD
-      this.http
-        .post("http://mongo-proxy.healthcreek.org/token",
-          body, {
-            headers: headers
-          })
-          .map(response => response.json())
-          .subscribe(data => {
-                console.log("data:"+JSON.stringify(data));
-                console.log(data.access_token);
-          });
-=======
       return this.http.post("http://mongo-proxy.healthcreek.org/token",body, {headers: headers}).map(response => response.json());
->>>>>>> Dev
     }
     requestAuth() {
       //encodeURI

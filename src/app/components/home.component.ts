ff417d6caa99e6c0571e8361fd8702d54ebe0962
Preston Lee <preston.lee@prestonlee.com>
2017-05-19 08:57:08 -0700

    Working through layout quirks.

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -23,10 +23,10 @@ export class HomeComponent {
 		'min_rows': 1,              //  The minimum number of rows allowed. Can be any number greater than or equal to 1.
 		'col_width': 2,           //  The width of each column
 		'row_height': 2,          //  The height of each row
-		'cascade': 'none',            //  The direction to cascade grid items ('up', 'right', 'down', 'left')
-		'min_width': 100,           //  The minimum width of an item. If greater than col_width, this will update the value of min_cols
-		'min_height': 100,          //  The minimum height of an item. If greater than row_height, this will update the value of min_rows
-		'fix_to_grid': true,       //  Fix all item movements to the grid
+		'cascade': 'up',            //  The direction to cascade grid items ('up', 'right', 'down', 'left')
+		'min_width': 1,           //  The minimum width of an item. If greater than col_width, this will update the value of min_cols
+		'min_height': 1,          //  The minimum height of an item. If greater than row_height, this will update the value of min_rows
+		'fix_to_grid': false,       //  Fix all item movements to the grid
 		'auto_style': true,         //  Automatically add required element styles at run-time
 		'auto_resize': false,       //  Automatically set col_width/row_height so that max_cols/max_rows fills the screen. Only has effect is max_cols or max_rows is set
 		'maintain_ratio': false,    //  Attempts to maintain aspect ratio based on the colWidth/rowHeight values set in the config
--- src/app/components/toolbar.component.ts
+++ src/app/components/toolbar.component.ts
@@ -20,38 +20,33 @@ import {Patient} from '../models/patient.model';
 	// 	])
 	// ]
 })
-export class ToolbarComponent implements DraggableWidget {
+export class ToolbarComponent {
 
 	@Input() patient: Patient;
 
     nav2: boolean = false;
 
 	// angular2-grid: https://github.com/BTMorton/angular2-grid
-	gridItemConfiguration: NgGridItemConfig =
-	{
-		'dragHandle': '.btn',
-		'col': 200, 'row': 1, 'sizex': 1, 'sizey': 1
+	gridItemConfiguration: NgGridItemConfig = {
+		'col': 50,               //  The start column for the item
+		'row': 1,               //  The start row for the item
+		'sizex': 40,             //  The start width in terms of columns for the item
+		'sizey': 4,             //  The start height in terms of rows for the item
+		'dragHandle': null,     //  The selector to be used for the drag handle. If null, uses the whole item
+		'resizeHandle': null,   //  The selector to be used for the resize handle. If null, uses 'borderSize' pixels from the right for horizontal resize,
+		//    'borderSize' pixels from the bottom for vertical, and the square in the corner bottom-right for both
+		'borderSize': 15,
+		'fixed': false,         //  If the grid item should be cascaded or not. If yes, manual movement is required
+		'draggable': true,      //  If the grid item can be dragged. If this or the global setting is set to false, the item cannot be dragged.
+		'resizable': false      //  If the grid item can be resized. If this or the global setting is set to false, the item cannot be resized.
+		// 'payload': null,        //  An optional custom payload (string/number/object) to be used to identify the item for serialization
+		// 'maxCols': 38,           //  The maximum number of columns for a particular item. This value will only override the value from the grid (if set) if it is smaller
+		// 'minCols': 38,           //  The minimum number of columns for a particular item. This value will only override the value from the grid if larger
+		// 'maxRows': 8,           //  The maximum number of rows for a particular item. This value will only override the value from the grid (if set) if it is smaller
+		// 'minRows': 8,           //  The minimum number of rows for a particular item. This value will only override the value from the grid if larger
+		// 'minWidth': 0,          //  The minimum width of a particular item. This value will override the value from the grid, as well as the minimum columns if the resulting size is larger
+		// 'minHeight': 0,         //  The minimum height of a particular item. This value will override the value from the grid, as well as the minimum rows if the resulting size is larger
 	}
-	// {
-	// 'col': 3,               //  The start column for the item
-	// 'row': 1,               //  The start row for the item
-	// 'sizex': 2,             //  The start width in terms of columns for the item
-	// 'sizey': 1,             //  The start height in terms of rows for the item
-	// 'dragHandle': null,     //  The selector to be used for the drag handle. If null, uses the whole item
-	// 'resizeHandle': null,   //  The selector to be used for the resize handle. If null, uses 'borderSize' pixels from the right for horizontal resize,
-	//    'borderSize' pixels from the bottom for vertical, and the square in the corner bottom-right for both
-	// 'borderSize': 15,
-	// 'fixed': false,         //  If the grid item should be cascaded or not. If yes, manual movement is required
-	// 'draggable': true,      //  If the grid item can be dragged. If this or the global setting is set to false, the item cannot be dragged.
-	// 'resizable': true,      //  If the grid item can be resized. If this or the global setting is set to false, the item cannot be resized.
-	// 'payload': null,        //  An optional custom payload (string/number/object) to be used to identify the item for serialization
-	// 'maxCols': 0,           //  The maximum number of columns for a particular item. This value will only override the value from the grid (if set) if it is smaller
-	// 'minCols': 0,           //  The minimum number of columns for a particular item. This value will only override the value from the grid if larger
-	// 'maxRows': 0,           //  The maximum number of rows for a particular item. This value will only override the value from the grid (if set) if it is smaller
-	// 'minRows': 0,           //  The minimum number of rows for a particular item. This value will only override the value from the grid if larger
-	// 'minWidth': 0,          //  The minimum width of a particular item. This value will override the value from the grid, as well as the minimum columns if the resulting size is larger
-	// 'minHeight': 0,         //  The minimum height of a particular item. This value will override the value from the grid, as well as the minimum rows if the resulting size is larger
-	// }
 
 	constructor() {
 		console.log("ToolbarComponent created...");

081157e239b4b9e486b7c72945a8738860a25c78
kaaniboy <kaanaksoyaz@gmail.com>
2018-03-28 14:20:37 -0700

    Begin cleaning AllergyHover

--- src/app/components/allergyHover.component.ts
+++ src/app/components/allergyHover.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, ElementRef, Injectable} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
-import { PatientComponent } from '../components/patient.component';
 
 @Component({
     selector: 'allergyHover',
@@ -13,40 +12,26 @@ import { PatientComponent } from '../components/patient.component';
 export class AllergyHoverComponent {
     // // The data passed into the menu from wherever it was triggered.
     //allergies doesn't need this. we can import data directly into this component
-    data: any = null;
+    items: Array<any> = null;
     //
     visible: boolean = false;
     top: string = '0px';
     left: string = '0px';
-    allAllergies2: string = this.patientComponent.allAllergies;
 
-    // Observable subscription to document hover
-    hoverSubscription: any = null;
+    constructor(private ref: ElementRef) { }
 
-    constructor(
-        private ref: ElementRef,
-        private patientComponent: PatientComponent) { }
-
-    private handleDocHover(event) {
-        // Hide the menu if the user hovers outside of it.
-        if (!this.ref.nativeElement.contains(event.target)) {
-            this.hide(null);
-        }
+    public handleItemClick(event) {
+      event.preventDefault();
     }
+
     //REMOVE EXTRANEOUS CODE COPIED FROM CONTEXTMENU.COMPONENT.TS
     // Show the menu.
-    public show(data, event) {
-        this.data = data; //set data to allergies by passing in allergy as a param of the funct when called in the pug
+    public show(items, event) {
+        this.items = items;
 
         // Set the location of the textbox to where the user clicked.
         this.top = event.pageY + 'px';
         this.left = event.pageX + 'px';
-
-        // Subscribe to document click events.
-        this.hoverSubscription = Observable.fromEvent(document, 'hover')
-            .subscribe((event) => {
-                this.handleDocHover(event);
-            });
 
         this.visible = true;
     }
@@ -57,12 +42,8 @@ export class AllergyHoverComponent {
             event.preventDefault();
         }
 
-        // Unsubscribe from document click events.
-        this.hoverSubscription.unsubscribe();
-        this.hoverSubscription = null;
-
         // Hide the menu and reset the associated data.
         this.visible = false;
-        this.data = null;
+        this.items = null;
     }
 }

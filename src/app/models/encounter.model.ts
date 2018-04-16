8754b7e9e3d9764b9f884b458c74f48abc068f88
kaaniboy <kaanaksoyaz@gmail.com>
2018-04-15 17:49:23 -0700

    Fix encountersService and add Encounter model to support reason for visit

---
+++ src/app/models/encounter.model.ts
@@ -0,0 +1,37 @@
+export class Encounter {
+    id: string;
+    status: string;
+
+    period: {
+      start: string;
+      end: string;
+    };
+
+    // Reasons for the encounter.
+    reason: {
+      text: string;
+
+      // Coding details for the encounter reason.
+      coding: {
+        system: string;
+        code: string;
+      }[];
+    }[];
+
+    // Get the reason for the encounter (if there is one). Otherwise, return null.
+    getReason() {
+      // Check if there are any reasons. If not, return null.
+      if (!this.reason) {
+        return null;
+      }
+
+      for (let r of this.reason) {
+        if (r.text) {
+          // Return the text associated with the reason.
+          return r.text;
+        }
+      }
+
+      return null;
+    }
+};
--- src/app/services/encounter.service.ts
+++ src/app/services/encounter.service.ts
@@ -1,7 +1,8 @@
 import { Component, Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { Patient } from '../models/patient.model';
+import { Encounter } from '../models/encounter.model';
 import { FhirService } from './fhir.service';
 
 @Injectable()
@@ -11,13 +12,10 @@ export class EncounterService {
 
   constructor(private fhirService: FhirService, private http: Http) { }
 
-  index() {
-    var url = this.fhirService.getUrl() + this.path;
-    return this.http.get(url).map(res => res.json());
-  }
-
-  get(id) {
-    var url = this.fhirService.getUrl() + this.path + '/' + id;
-    return this.http.get(url).map(res => res.json());
+  // TODO: Currently only retrieves 10 encounters. Need to add pagination support.
+  loadEncounters(patient: Patient) {
+    var url = this.fhirService.getUrl() + this.path + "?patient=" + patient.id;
+    return this.http.get(url, this.fhirService.options(true))
+        .map(res => res.json()['entry'].map(e => <Encounter> e['resource']));
   }
 }

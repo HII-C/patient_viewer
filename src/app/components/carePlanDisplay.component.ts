5827f5bce6e6c04f62ec25592910310a7300480a
kaaniboy <kaanaksoyaz@gmail.com>
2018-04-18 13:59:09 -0700

    Fix contextmenu in all three columns

--- src/app/components/carePlanDisplay.component.ts
+++ src/app/components/carePlanDisplay.component.ts
@@ -14,93 +14,96 @@ import { ContextMenuComponent } from './contextMenu.component';
 
 
 @Component({
-    selector: 'careplanDisplay',
-    templateUrl: '/carePlanDisplay.html'
+  selector: 'careplanDisplay',
+  templateUrl: '/carePlanDisplay.html'
 })
 export class CarePlanDisplay {
-    // The currently selected careplan in the list.
-    selected: CarePlan;
+  // The currently selected careplan in the list.
+  selected: CarePlan;
 
-    // This is the array of careplans to be displayed
-    @Input() carePlans: Array<CarePlan>;
-    @Output() careplanSelected: EventEmitter<CarePlan> = new EventEmitter();
+  // This is the array of careplans to be displayed
+  @Input() carePlans: Array<CarePlan>;
+  @Output() careplanSelected: EventEmitter<CarePlan> = new EventEmitter();
 
-    @ViewChild('menu') menu: ContextMenuComponent;
+  @ViewChild('menu') menu: ContextMenuComponent;
 
 
-    // ===============================================================================================================================================
-    // ================================================================== EVENT METHODS ==============================================================
-    // ==================================================================---------------==============================================================
+  // ===============================================================================================================================================
+  // ================================================================== EVENT METHODS ==============================================================
+  // ==================================================================---------------==============================================================
 
-    constructor(private scratchPadService: ScratchPadService){}
+  constructor(private scratchPadService: ScratchPadService) { }
 
-    ngOnInit() {}
+  ngOnInit() { }
 
-    //=================================================================== CONTEXT MENU ==============================================================
+  //=================================================================== CONTEXT MENU ==============================================================
 
-    // Can only access view child after the view has been initialized.
-    ngAfterViewInit() {
-        // Add options to the context menu shown when right clicking careplans.
-        this.menu.addOption({
-            'icon': 'glyphicon-pencil',
-            'text': 'Add to Scratch Pad',
-            'exec': function(data) {
-                this.scratchPadService.addCarePlan(data);
-            }
-        });
+  // Can only access view child after the view has been initialized.
+  ngAfterViewInit() {
+    // NOTE: 'exec' functions must be bound to 'this' to access scratchPadService.
+    // This is a strange behavior with scoping in Typescript/Javascript.
 
+    // Add options to the context menu shown when right clicking careplans.
     this.menu.addOption({
-        'icon': 'glyphicon-stats',
-        'text': 'Add to Trend Tool',
-        'exec': function(data) {
-            console.log(data);
-        }
+      'icon': 'glyphicon-pencil',
+      'text': 'Add to Scratch Pad',
+      'exec': function(plan) {
+        this.scratchPadService.addCarePlan(plan);
+      }.bind(this)
     });
 
     this.menu.addOption({
-        'icon': 'glyphicon-random',
-            'text': 'Open Association Tool',
-            'exec': function(data) {
-                console.log(data);
-            }
-        });
+      'icon': 'glyphicon-stats',
+      'text': 'Add to Trend Tool',
+      'exec': function(plan) {
+        console.log(plan);
+      }.bind(this)
+    });
+
+    this.menu.addOption({
+      'icon': 'glyphicon-random',
+      'text': 'Open Association Tool',
+      'exec': function(plan) {
+        console.log(plan);
+      }.bind(this)
+    });
+  }
+
+  // FOR MAINTAINING CHECK STATE AFTER LOSING FOCUS
+
+  //whenver a line is selected
+  selectCarePlan(carePlan: CarePlan) {
+    this.selected = carePlan;
+    this.careplanSelected.emit(this.selected);
+    for (let c of this.scratchPadService.totalCareplans) {
+      c['selected'] = (c.id == this.selected.id);
+    }
+  }
+
+  checkClicked(carePlan: CarePlan) {
+    if (this.scratchPadService.checkedMapCareplans.get(carePlan)) {
+      return true;
     }
 
-    // FOR MAINTAINING CHECK STATE AFTER LOSING FOCUS
+    return false;
+  }
 
-    //whenver a line is selected
-    selectCarePlan(carePlan: CarePlan) {
-        this.selected = carePlan;
-        this.careplanSelected.emit(this.selected);
-        for (let c of this.scratchPadService.totalCareplans) {
-            c['selected'] = (c.id == this.selected.id);
+  // WHENEVER A CHECKBOX IS CLICKED OR UNCLICKED, IT REGISTERS IT IN THE SCRATCHPADSERVICE (not actually the scratch pad yet)
+  checkCarePlan(checked: boolean, checkedCarePlan: CarePlan) {
+    this.scratchPadService.checkCarePlan(checked, checkedCarePlan);
+  }
+
+  expand(parent: string) {
+    /*
+    for (let c of this.carePlans) {
+        if (c.parent == parent) {
+            c.isVisible = true;
+            c.parent = "";
+            c.isParent = false;
         }
     }
+    */
+  }
 
-    checkClicked(carePlan: CarePlan) {
-        if (this.scratchPadService.checkedMapCareplans.get(carePlan)){
-            return true;
-        }
-
-        return false;
-    }
-
-    // WHENEVER A CHECKBOX IS CLICKED OR UNCLICKED, IT REGISTERS IT IN THE SCRATCHPADSERVICE (not actually the scratch pad yet)
-    checkCarePlan(checked: boolean, checkedCarePlan: CarePlan) {
-        this.scratchPadService.checkCarePlan(checked, checkedCarePlan);
-    }
-
-    expand(parent: string) {
-        /*
-        for (let c of this.carePlans) {
-            if (c.parent == parent) {
-                c.isVisible = true;
-                c.parent = "";
-                c.isParent = false;
-            }
-        }
-        */
-    }
-    
 
 }

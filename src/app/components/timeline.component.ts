e72c9f0f2bfb02bcea48aa01ebdc1d02e7d2b611
Matthew Budiman <mattbudiman38@gmail.com>
2019-11-25 23:22:36 -0700

    Clean up code

--- src/app/components/timeline.component.ts
+++ src/app/components/timeline.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input } from '@angular/core';
-
-import { FhirService } from '../services/fhir.service';
-import { TimelineService } from '../services/timeline.service';
 import { EncounterService } from '../services/encounter.service';
-import { DoctorService } from '../services/doctor.service';
-import { Timeline } from '../models/timeline.model';
 import { Encounter } from '../models/encounter.model';
 import { Patient } from '../models/patient.model';
 
@@ -13,34 +8,14 @@ import { Patient } from '../models/patient.model';
   templateUrl: '/timeline.html'
 })
 export class TimelineComponent {
-  selected: Timeline;
-  timeline: Array<Timeline> = [];
   encounters: Array<Encounter> = [];
   @Input() patient: Patient;
 
   // For options: https://github.com/BTMorton/angular2-grid
-  constructor(
-    private fhirService: FhirService,
-    private timelineService: TimelineService,
-    private encounterService: EncounterService,
-    private doctorService: DoctorService
-  ) { }
+  constructor(private encounterService: EncounterService) { }
 
-  ngOnChanges() {
-    if (this.patient) {
-      this.loadEncounters();
-      this.timelineService.index(this.patient).subscribe(data => {
-        if (data.entry) {
-          this.timeline = <Array<Timeline>>data.entry.map(r => r['resource']);
-          this.timeline = this.timeline.reverse();
-          console.log("Loaded " + this.timeline.length + " timelines.");
-          console.log(this.timeline);
-        } else {
-          this.timeline = new Array<Timeline>();
-          console.log("No timelines for patient.");
-        }
-      });
-    }
+  ngOnChanges(): void {
+    this.loadEncounters();
   }
 
   private loadEncounters(): void {

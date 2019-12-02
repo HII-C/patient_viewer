16c6a5034b7f36ba9bb4fbeb482d048d792340f0
Ajide49 <a.usman9098@gmail.com>
2019-12-02 00:50:55 -0700

    add date piping to hover boxes

--- src/app/components/timeline_popup.component.ts
+++ src/app/components/timeline_popup.component.ts
@@ -1,17 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { HoverBoxComponent } from '../components/hoverBox.component';
 import { Encounter } from '../models/encounter.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'timeline-popup',
   templateUrl: '/timeline_popup.html'
 })
+
 export class TimelinePopupComponent {
   @ViewChild('hoverBox') hoverBox: HoverBoxComponent;
-
+   datePipe: DatePipe = new DatePipe('en-US');
   open(encounter: Encounter, event: MouseEvent): void {
+   
     let details: Array<string> = [
-      'Period: ' + encounter.period.start + ' to ' + encounter.period.end,
+      'Date: ' + this.datePipe.transform(encounter.getStartDate(),'MM-dd-yyyy').toString(),
       'Reason: ' + (encounter.getReason() || 'None')
     ];
     this.hoverBox.show(details, event);
--- src/app/models/encounter.model.ts
+++ src/app/models/encounter.model.ts
@@ -1,37 +1,44 @@
 export class Encounter {
-    id: string;
+  id: string;
+  status: string;
+
+  period: {
+    start: string;
+    end: string;
+  };
+
+  text: {
     status: string;
+    div: string;
+  }
 
-    period: {
-      start: string;
-      end: string;
-    };
+  // Reasons for the encounter.
+  reason: {
+    text: string;
 
-    // Reasons for the encounter.
-    reason: {
-      text: string;
-
-      // Coding details for the encounter reason.
-      coding: {
-        system: string;
-        code: string;
-      }[];
+    // Coding details for the encounter reason.
+    coding: {
+      system: string;
+      code: string;
     }[];
+  }[];
 
-    // Get the first reason for the encounter (if there is one). Otherwise, return null.
-    getReason() : string {
-      // Check if there are any reasons. If not, return null.
-      if (!this.reason) {
-        return null;
-      }
-
-      for (let r of this.reason) {
-        if (r.text) {
-          // Return the text associated with the reason.
-          return r.text;
-        }
-      }
-
+  // Get the first reason for the encounter (if there is one). Otherwise, return null.
+  getReason(): string {
+    // Check if there are any reasons. If not, return null.
+    if (!this.reason) {
       return null;
     }
+
+    for (let r of this.reason) {
+      if (r.text) {
+        // Return the text associated with the reason.
+        return r.text;
+      }
+    }
+
+    return null;
+  }
+  getStartDate(): Date { return new Date(this.period.start)}
+  getEndDate(): Date { return new Date(this.period.end)}
 };

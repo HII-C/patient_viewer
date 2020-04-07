72d09f9cf362f864b61ad0291e9f7c0afcc901ef
Matthew Budiman <mattbudiman38@gmail.com>
2020-04-06 19:33:52 -0500

    Document Encounter service

--- src/app/services/encounter.service.ts
+++ src/app/services/encounter.service.ts
@@ -9,7 +9,10 @@ import { Bundle } from '../models/bundle.model';
 import { Patient } from '../models/patient.model';
 import { Encounter } from '../models/encounter.model';
 
-
+/**
+ * A service for retrieving patient encounters
+ * from the FHIR server
+ */
 @Injectable()
 export class EncounterService {
   private path = '/Encounter';
@@ -19,6 +22,10 @@ export class EncounterService {
     private fhirService: FhirService
   ) { }
 
+  /**
+   * Load a page of encounters
+   * @param url Endpoint from which to retrieve encounters
+   */
   private loadEncountersPage(url: string): Observable<Array<Encounter>> {
     return this.http.get<Bundle>(url, this.fhirService.getRequestOptions())
       .pipe(concatMap(bundle => {
@@ -36,14 +43,23 @@ export class EncounterService {
       }));
   }
 
+  /**
+   * Retrieve encounters for a given patient from the FHIR server.
+   * @param patient The patient whose encounters to retrieve
+   */
   public loadEncounters(patient: Patient): Observable<Array<Encounter>> {
     const url = this.fhirService.getUrl() + this.path + "?patient=" + patient.id;
     return this.loadEncountersPage(url);
   }
 
-  // We cannot simply cast the JSON object to an Encounter, because this casted
-  // Encounter will not have the methods of the Encounter class.
+  /**
+   * Convert a JSON encounter object from the FHIR server
+   * to an instance of the Encounter class
+   * @param input JSON encounter object from FHIR server
+   */
   private deserialize(input: any): Encounter {
+    // We cannot simply cast the JSON object to an Encounter, because this casted
+    // Encounter will not have the methods of the Encounter class.
     var fixed: Encounter = new Encounter();
 
     for (let prop in input) {

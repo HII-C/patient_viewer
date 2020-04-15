2f84daa4180908b7d290d61efff6467a40145bf6
kaaniboy <kaaniboy@users.noreply.github.com>
2020-04-15 23:42:25 +0000

    deploy: 34c9f5f9d14421c50b7bd34025fef1629e123858

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -372,6 +372,12 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
                             <li class="link">
+                                <a href="miscellaneous/enumerations.html" data-type="entity-link">Enums</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
                                 <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
                             </li>
                         </ul>

2514a8ac0653334b5db90c9622e4ed551aa3b12d
Ajide49 <a.usman9098@gmail.com>
2020-04-28 14:27:00 -0700

    Fix load encounters in timeline popup

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -20,22 +20,22 @@ export class TimelineComponent {
   }
 
   private loadEncounters(): void {
+    console.log('patient1',this.patient);
     if (this.patient) {
       this.encounterService.loadEncounters(this.patient).subscribe(encounters => {
         this.encounters = this.encounters.concat(encounters);
-        console.log('Loaded ' + this.encounters.length + ' encounters.');
-        console.log(this.encounters);
+        // console.log('Loaded ' + this.encounters.length + ' encounters.');
+        // console.log(this.encounters);
 
-        encounters.forEach(enc => enc.position = enc.getLogValue()/5 + "%")
-        encounters.forEach(enc =>  console.log('Log Val pos: ' + enc.getStartDate()))
+        // encounters.forEach(enc => enc.position = enc.getLogValue()/5 + "%")
+        // encounters.forEach(enc =>  console.log('Log Val pos: ' + enc.getStartDate()))
         
       });
     }
   }
   private onScrollClick(): void{
-        // if(forward){
+    console.log('patient2',this.patient);
 
-          console.log("hello");
-        // }
+         console.log("e isz : ",this.encounters[1]);
   }
 }
--- src/app/components/timeline_popup/timeline_popup.component.ts
+++ src/app/components/timeline_popup/timeline_popup.component.ts
@@ -1,9 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 import { HoverBoxComponent } from '../../components/hoverBox/hoverBox.component';
+import { EncounterService } from '../../services/encounter.service';
 
 import { Encounter } from '../../models/encounter.model';
+import { Patient } from '../../models/patient.model';
 
 @Component({
   selector: 'timeline-popup',
@@ -12,17 +14,47 @@ import { Encounter } from '../../models/encounter.model';
 
 export class TimelinePopupComponent {
   @ViewChild('hoverBox', { static: false }) hoverBox: HoverBoxComponent;
+  @Input() patient: Patient;
+
+  constructor(private encounterService: EncounterService) { }
+
    datePipe: DatePipe = new DatePipe('en-US');
-  open(encounter: Encounter, event: MouseEvent): void {
+   encounters: Array<Encounter> = [];
+   e: Encounter;
+   details : Array<String>;
+
+   ngOnChanges(){
+     this.loadEncounters();
+   }
    
-    let details: Array<string> = [
-      'Date: ' + this.datePipe.transform(encounter.getStartDate(),'MM-dd-yyyy').toString(),
-      'Reason: ' + (encounter.getReason() || 'None')
-    ];
+  open(index : any, event: MouseEvent): void {
+    console.log('patient3',this.patient);
+    console.log("e is here like : ",this.encounters[index]);
+    let details: Array<String>  = [
+      'Date: ' + this.datePipe.transform(this.encounters[index].getStartDate(),'MM-dd-yyyy').toString(),
+      'Reason: ' + (this.encounters[index].getReason() || 'None')
+   ];
+ 
     this.hoverBox.show(details, event);
+     
   }
 
   close(event: MouseEvent): void {
     this.hoverBox.hide(event);
   }
+
+  private loadEncounters(){
+    if (this.patient) {
+      this.encounterService.loadEncounters(this.patient).subscribe(encounters => {
+        this.encounters = this.encounters.concat(encounters);
+        console.log('Loaded ' + this.encounters.length + ' encounters.');
+        console.log(this.encounters);
+
+        encounters.forEach(enc => enc.position = enc.getLogValue()/5 + "%")
+        encounters.forEach(enc =>  console.log('Log Val pos: ' + enc.getStartDate()))
+      });
+    }
+
+  }
 }
+
